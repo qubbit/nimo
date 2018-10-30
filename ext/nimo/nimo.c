79e9c6d52a456e1c873d652d7eee8aea79da8414
qubbit <gopala@email.arizona.edu>
2018-10-30 19:53:47 -0400

    Fix native macOS function call for display_alert_panel

--- ext/nimo/nimo.c
+++ ext/nimo/nimo.c
@@ -7,7 +7,15 @@
 // void rb_define_singleton_method(VALUE object, const char *name,
 //                                 VALUE (*func)(ANYARGS), int argc)
 
-VALUE display_alert_panel(T_STRING title, T_STRING message) {
+VALUE display_alert_panel(VALUE self, VALUE title, VALUE message) {
+  Check_Type(title, T_STRING);
+  Check_Type(message, T_STRING);
+
+  char* title_ptr = RSTRING_PTR(title);
+  char* message_ptr = RSTRING_PTR(message);
+
+  printf("title: %s, message: %s\n", title_ptr, message_ptr);
+
   id app = NULL;
   id pool = (id)objc_getClass("NSAutoreleasePool");
   if (!pool) {
@@ -24,15 +32,13 @@ VALUE display_alert_panel(T_STRING title, T_STRING message) {
   app = objc_msgSend((id)objc_getClass("NSApplication"),
                      sel_registerName("sharedApplication"));
 
-  NSRunAlertPanel(CFSTR(title), CFSTR(message), CFSTR("OK"), NULL, NULL);
+  NSRunAlertPanel(CFStringCreateWithCString(NULL, title_ptr, kCFStringEncodingUTF8), CFStringCreateWithCString(NULL, message_ptr, kCFStringEncodingUTF8), CFSTR("OK"), NULL, NULL);
   objc_msgSend(pool, sel_registerName("release"));
 }
 
 VALUE nimo_hello() {
   printf("Hello, World!\n");
   // setDesktopImageURL("/Users/gadhikari/Wallpapers/eXpresso.jpg", 1, NULL);
-  printf("%p", pool);
-
   return Qnil;
 }
 
@@ -40,5 +46,5 @@ void Init_nimo(void) {
   VALUE cNimo = rb_define_class("Nimo", rb_cObject);
 
   rb_define_singleton_method(cNimo, "hello", nimo_hello, 0);
-  rb_define_singleton_method(cNimo, "display_alert_message", nimo_hello, 2);
+  rb_define_singleton_method(cNimo, "display_alert_panel", display_alert_panel, 2);
 }
